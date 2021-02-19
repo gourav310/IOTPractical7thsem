193f2a587541c0a16bfa3cee557cd10429b7181e
gourav310 <67389253+gourav310@users.noreply.github.com>
2021-02-19 11:03:11 +0530

    Add files via upload

---
+++ print_n_times.py
@@ -0,0 +1,11 @@
+# name - Gourav khurana 
+# Roll no - 17EEBCS028
+# Iot lab Examination 
+# Date- 19 Feb 2021 
+
+name = (input("PLease enter name: "))
+n = (int)(input("Enter n: "))
+
+for i in range(0,n):
+    print(name)
+
---
+++ time.py
@@ -0,0 +1,9 @@
+
+# name - Gourav khurana 
+# Roll no - 17EEBCS028
+# Iot lab Examination 
+# Date- 19 Feb 2021 
+import time
+for i in range(1,10):
+    print(time.ctime())
+    time.sleep(10)
